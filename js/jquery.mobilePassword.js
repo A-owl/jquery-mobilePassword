66fd205bd4e3fec34b36fabc37500712e84f0da9
MarvelousOwl <marvelousowl@gmail.com>
2014-06-24 11:43:42 +0800

    changge str..

--- js/jquery.mobilePassword.js
+++ js/jquery.mobilePassword.js
@@ -10,119 +10,146 @@
  *
  */
 ;
-(function($) {
-  $.fn.mobilePassword = function(options) {
-    var opts = $.extend({}, $.fn.mobilePassword.defaults, options);
+(function ($) {
+    var defaults = {
+        checkInterval: 200, //set timeout to check whether all the characters are the same
+        transDelay   : 200, //delay to transform last letter
+        character    : '%u25CF', //instead of the character
+        callback     : null
+    };
 
-    var checkTimeout = [];
-    var transTimeout = [];
-    var regex = new RegExp('[^' + opts.character + ']', 'gi');
-    var i = 0;
-
-    this.each(function(index) {
-      var suffix = 'Clone' + index;
-      var $this = $(this);
-      var id = this.id;
-      var name = this.name;
-      var newId = id + suffix;
-      var newName = id + suffix;
-      if (!id || !name) {
-        alert("Please set 'id' and 'name' attributes for elements!");
-        return false;
-      }
-      var $newObj = $('<input>')
-        .attr($.extend(extractAttributes(this), { 'id': newId, 'name': newName, 'autocomplete': 'off', 'type': 'text'}))
-      var newElem = $('#' + newId)[0];
-      $this.after($newObj).attr({'accessKey':'',tabIndex:''}).hide();
-      $newObj.on('focus', function() {
-        var oldValue = $newObj.val();
-        checkTimeout[index] = setTimeout(function() {
-          checkChange(index, id, newId, oldValue);
-        }, opts.checkInterval);
-      });
-      $newObj.on('blur', function() {
-        charReplace(index, newId);
-        clearTimeout(checkTimeout[index]);
-      });
-      $('label').each(function() {
-        if (this.htmlFor == id) {
-          this.htmlFor = newId
-        } else if ($this[0].parentNode.tagName.toLowerCase() == 'label') {
-          $this[0].parentNode.htmlFor = newId
-        }
-      })
-    });
-
-    // Return an object of element attributes
-    function extractAttributes(elem) {
-      var attr = elem.attributes,
-        copy = {}, skip = /^jQuery\d+/;
-      for (var i = 0; i < attr.length; i++) {
-        if (attr[i].specified && !skip.test(attr[i].name)) {
-          copy[attr[i].name] = attr[i].value;
-        }
-      }
-      return copy;
+    /**
+     * The actual plugin constructor
+     * @param $elem
+     * @param options
+     * @constructor
+     */
+    function Password($elem, options) {
+        this.$elem = $elem;
+        this.options = options;
+        this.transTimeout = null;
+        this.checkTimeout = null;
+        this.init();
+        return this;
     }
 
+    /**
+     * Initialization
+     */
+    Password.prototype.init = function () {
+        var that = this;
+
+        var suffix = 'Clone';
+
+        var id = this.$elem[0].id || '';
+        var name = this.$elem[0].name || '';
+        var newId = id + suffix;
+        var newName = name + suffix;
+
+        this.$newElem = $('<input>');
+        this.$newElem.attr($.extend(extractAttributes(this.$elem[0]), {
+                'id'          : newId,
+                'name'        : newName,
+                'autocomplete': 'off',
+                'type'        : 'text',
+                'ime-mode'    : 'disabled'
+            }));
+
+        this.$elem.after(this.$newElem).attr({'accessKey': '', tabIndex: ''}).hide();
+
+
+        this.$newElem.on('focus', function () {
+//            that.checkTimeout = setTimeout(function () {
+//                that._checkChange();
+//            }, that.options.checkInterval);
+            that._checkChange();
+        });
+        this.$newElem.on('blur', function () {
+            that._charReplace();
+            clearTimeout(that.checkTimeout);
+        });
+
+//        $('label').each(function () {
+//            if ($(this)[0].htmlFor == id) {
+//                $(this)[0].htmlFor = newId;
+//            } else if (that.$elem[0].parentNode.tagName.toLowerCase() == 'label') {
+//                that.$elem.parentNode.htmlFor = newId;
+//            }
+//        });
+        if( this.options.callback && typeof this.options.callback ==='function'){
+            this.options.callback.call(this);
+        }
+    };
+
+    Password.prototype.destroy = function (){
+        this.$newElem.remove();
+        this.$newElem = null;
+        this.$elem.show();
+        clearTimeout(this.checkTimeout);
+        clearTimeout(this.transTimeout);
+        return this;
+    };
+
     /*
-     * check whether all the characters are the same
+     * check whether all the characters is changed
      * eg. user input and paste the characters
      */
-    function checkChange(index, oldId, id, oldValue) {
-      var curValue = $('#' + id).val();
-      if (curValue != oldValue) {
-        saveString(index, oldId, id);
-      } else {
-        charReplace(index, id);
-      }
-      oldValue = curValue;
-      checkTimeout[index] = setTimeout(function() {
-        checkChange(index, oldId, id, oldValue);
-      }, opts.checkInterval)
+    Password.prototype._checkChange =  function (oldValue) {
+        var that = this;
+        var curValue = this.$newElem.val();
+        if (curValue != oldValue) {
+            that._saveString();
+        } else {
+            that._charReplace();
+        }
+        oldValue = curValue;
+        that.checkTimeout = setTimeout(function () {
+            that._checkChange(oldValue);
+        }, that.options.checkInterval);
     };
+
 
     /*
      * save string from new input
      */
-    function saveString(index, oldId, id) {
-      var pos = getCurPos(id);
-      var lastInputChar = false;
-      var inpObj = $('#' + id);
-      var passObj = $('#' + oldId);
-      var inputChars = inpObj.val().split('');
-      var passChars = passObj.val().split('');
-      if (transTimeout[index]) {
-        clearTimeout(transTimeout[index]);
-        transTimeout[index] = null
-      }
-      for (i = 0; i < inputChars.length; i++) {
-        if (inputChars[i] != passChars[i]) {
-          if (inputChars[i] != unescape(opts.character)) {
-            passChars.splice(i, 0, inputChars[i])
-          } else {
-            passChars[i] = passChars[i]
-          }
-        } else {
-          passChars.splice(i, 0, inputChars[i])
+    Password.prototype._saveString = function() {
+        var pos = getCurPos(this.$newElem[0]);
+        var lastInputChar = false;
+        var inpObj = this.$newElem;
+        var passObj =this.$elem;
+        var inputChars = inpObj.val().split('');
+        var passChars = passObj.val().split('');
+        if (this.transTimeout) {
+            clearTimeout(this.transTimeout);
+            this.transTimeout = null;
         }
-      }
-      if (inputChars.length < passChars.length) {
-        passChars.splice(pos.start, passChars.length - inputChars.length, '')
-      }
-      for (i = 0; i < inputChars.length; i++) {
-        if (inputChars[i] != unescape(opts.character)) {
-          lastInputChar = i
+        for (var i = 0; i < inputChars.length; i++) {
+            if (inputChars[i] != passChars[i]) {
+                if (inputChars[i] != unescape(this.options.character)) {
+                    passChars.splice(i, 0, inputChars[i])
+                } else {
+                    passChars[i] = passChars[i]
+                }
+            } else {
+                passChars.splice(i, 0, inputChars[i])
+            }
         }
-      }
-      for (i = 0; i < inputChars.length; i++) {
-        if (i < lastInputChar) {
-          inputChars[i] = unescape(opts.character)
+        if (inputChars.length < passChars.length) {
+            passChars.splice(pos.start, passChars.length - inputChars.length, '')
         }
-      }
-      inpObj.val(inputChars.join(''));
-      passObj.val(passChars.join(''));
-      setCurPos(id, pos)
+        for (i = 0; i < inputChars.length; i++) {
+            if (inputChars[i] != unescape(this.options.character)) {
+                lastInputChar = i
+            }
+        }
+        for (i = 0; i < inputChars.length; i++) {
+            if (i < lastInputChar) {
+                inputChars[i] = unescape(this.options.character)
+            }
+        }
+        inpObj.val(inputChars.join(''));
+        passObj.val(passChars.join(''));
+        setCurPos(this.$newElem[0], pos)
     };
 
     /*
@@ -131,57 +158,74 @@
      * You can find a good overview here:
      * http://www.fileformat.info/format/w3c/entitytest.htm?sort=Unicode+Character
      */
-    function charReplace(index, id) {
-      var pos = getCurPos(id);
-      var inpObj = $('#' + id);
-      var curValue = inpObj.val();
-      if (!transTimeout[index] && curValue.match(regex) != null) {
-        transTimeout[index] = setTimeout(function() {
-          inpObj.val(curValue.replace(regex, unescape(opts.character)));
-          setCurPos(id, pos)
-        }, opts.transDelay)
-      }
+    Password.prototype._charReplace = function () {
+        var that = this;
+        var regex = new RegExp('[^' + this.options.character + ']', 'gi');
+
+        var pos = getCurPos(this.$newElem[0]);
+        var curValue = this.$newElem.val();
+        if (!this.transTimeout && curValue.match(regex) != null) {
+            this.transTimeout = setTimeout(function () {
+                that.$newElem.val(curValue.replace(regex, unescape(that.options.character)));
+                setCurPos(that.$newElem[0], pos)
+            }, that.options.transDelay);
+        }
     };
 
-    function getCurPos(id) {
-      var input = $('#' + id)[0];
-      var pos = {
-        start: 0,
-        end: 0
-      };
-      if (input.setSelectionRange) {
-        pos.start = input.selectionStart;
-        pos.end = input.selectionEnd
-      } else if (input.createTextRange) {
-        var bookmark = document.selection.createRange().getBookmark();
-        var selection = input.createTextRange();
-        var before = selection.duplicate();
-        selection.moveToBookmark(bookmark);
-        before.setEndPoint('EndToStart', selection);
-        pos.start = before.text.length;
-        pos.end = pos.start + selection.text.length;
-      }
-      return pos;
-    };
-
-    function setCurPos(id, pos) {
-      var input = $('#' + id)[0];
-      if (input.setSelectionRange) {
-        input.setSelectionRange(pos.start, pos.end);
-      } else if (input.createTextRange) {
-        var selection = input.createTextRange();
-        selection.collapse(true);
-        selection.moveEnd('character', pos.end);
-        selection.moveStart('character', pos.start);
-        selection.select();
-      }
+    // Return an object of element attributes
+    function extractAttributes(elem) {
+        var attr = elem.attributes,
+            copy = {}, skip = /^jQuery\d+/;
+        for (var i = 0; i < attr.length; i++) {
+            if (attr[i].specified && !skip.test(attr[i].name)) {
+                copy[attr[i].name] = attr[i].value;
+            }
+        }
+        return copy;
     }
 
-  }
 
-  $.fn.mobilePassword.defaults = {
-    checkInterval: 200, //set timeout to check whether all the characters are the same
-    transDelay: 500, //delay to transform last letter
-    character: '%u25CF' //instead of the character
-  };
+    function getCurPos(input) {
+        var pos = {
+            start: 0,
+            end  : 0
+        };
+        if (input.setSelectionRange) {
+            pos.start = input.selectionStart;
+            pos.end = input.selectionEnd
+        } else if (input.createTextRange) {
+            var bookmark = document.selection.createRange().getBookmark();
+            var selection = input.createTextRange();
+            var before = selection.duplicate();
+            selection.moveToBookmark(bookmark);
+            before.setEndPoint('EndToStart', selection);
+            pos.start = before.text.length;
+            pos.end = pos.start + selection.text.length;
+        }
+        return pos;
+    }
+
+    function setCurPos(input, pos) {
+        if (input.setSelectionRange) {
+            input.setSelectionRange(pos.start, pos.end);
+        } else if (input.createTextRange) {
+            var selection = input.createTextRange();
+            selection.collapse(true);
+            selection.moveEnd('character', pos.end);
+            selection.moveStart('character', pos.start);
+            selection.select();
+        }
+    }
+
+
+    $.fn.mobilePassword = function (options) {
+        return this.each(function () {
+            var settings = $.extend({}, defaults, options || {});
+            if (!$.data(this, 'mobilePassword')) {
+                $.data(this, 'mobilePassword', new Password($(this), settings));
+            }
+        });
+    };
+
+
 })(jQuery);
